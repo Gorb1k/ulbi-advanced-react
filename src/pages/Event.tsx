483c06e2d6347d7ae4236862794682e0ded944df
Ihor Horb <gorbigorolegovich@gmail.com>
2022-06-07 13:44:50 +0300

    git action

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -24,7 +24,6 @@ const Event: FC = () => {
 
     return (
         <Layout>
-
             <EventCalendar events={events}/>
             <Row justify={"center"}>
                 <Button onClick={() => {
